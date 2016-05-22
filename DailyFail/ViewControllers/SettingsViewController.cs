4441f7e103bedff22d1db1deb9f5c47a12a89bee
Mike James <mike@xamarin.com>
2016-05-23 00:10:37 +0200

    Updating version info in settings

---
+++ DailyFail/ViewControllers/SettingsViewController.cs
@@ -0,0 +1,24 @@
+using Foundation;
+using System;
+using UIKit;
+
+namespace DailyFail
+{
+    public partial class SettingsViewController : UITableViewController
+    {
+        public SettingsViewController (IntPtr handle) : base (handle)
+        {
+        }
+
+        public override void ViewDidLoad()
+        {
+            base.ViewDidLoad();
+
+            var versionNumber = NSBundle.MainBundle.ObjectForInfoDictionary("CFBundleShortVersionString").ToString();
+            var buildNumber = NSBundle.MainBundle.ObjectForInfoDictionary("CFBundleVersion").ToString();
+
+
+            lblBuildVersion.Text = $"Version {versionNumber} build {buildNumber}";
+        }
+    }
+}
---
+++ DailyFail/ViewControllers/SettingsViewController.designer.cs
@@ -0,0 +1,29 @@
+// WARNING
+//
+// This file has been generated automatically by Xamarin Studio from the outlets and
+// actions declared in your storyboard file.
+// Manual changes to this file will not be maintained.
+//
+using Foundation;
+using System;
+using System.CodeDom.Compiler;
+using UIKit;
+
+namespace DailyFail
+{
+    [Register ("SettingsViewController")]
+    partial class SettingsViewController
+    {
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UILabel lblBuildVersion { get; set; }
+
+        void ReleaseDesignerOutlets ()
+        {
+            if (lblBuildVersion != null) {
+                lblBuildVersion.Dispose ();
+                lblBuildVersion = null;
+            }
+        }
+    }
+}
