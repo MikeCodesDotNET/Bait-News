be8aafb9ec22038910be75a54babfaebd68e0972
Mike James <mijam@microsoft.com>
2017-08-15 00:28:50 +0200

    Updated project structure

---
+++ DailyFail/ViewControllers/Home/HomeTableViewController.cs
@@ -0,0 +1,19 @@
+using Foundation;
+using System;
+using UIKit;
+
+using Google.Apis;
+
+namespace DailyFail
+{
+    public partial class HomeTableViewController : UITableViewController
+    {
+        public HomeTableViewController (IntPtr handle) : base (handle)
+        {
+        }
+
+		public override void ViewDidLoad()
+		{
+		}
+    }
+}
